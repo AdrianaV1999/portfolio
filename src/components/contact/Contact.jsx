7c2b3ad9ced1b5df02456c1d79ded43a809e2e14
Adriana <vadriana258@gmail.com>
2024-08-26 19:15:01 +0200

    Changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -107,7 +107,7 @@ const Contact = () => {
               style={{
                 width: "78%",
                 height: "auto",
-                marginLeft: isMobile ? "50px" : "135px",
+                marginLeft: isMobile ? "80px" : "135px",
               }}
             >
               <img
